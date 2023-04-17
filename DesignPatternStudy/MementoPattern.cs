c8e50a73c1c96f046628ba81b87e4e6c0638c60c
동윤 김 <ut_community@naver.com>
2023-04-17 16:18:51 +0900

    feat : MementoPatter Add
    객체를 이전 상태로 되돌릴 수 이쓴 기능을 제공하는 소프트웨어 디자인 패턴입니다. (롤백을 통한 실행 취소). 현재 프로젝트에서는 게임 데이터를 저장하는 용도로 만들었습니다.

--- DesignPatternStudy/MementoPattern.cs
+++ DesignPatternStudy/MementoPattern.cs
@@ -1,10 +1,86 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace DesignPatternStudy
 {
-    internal class MementoPattern
+    // Originator 클래스
+    class Game
     {
+        public int Level { get; set; }
+        public int Health { get; set; }
+        public int Score { get; set; }
+
+        public void DisplayStatus()
+        {
+            Console.WriteLine($"Level: {Level}, Health: {Health}, Score: {Score}");
+        }
+
+        // Memento 객체를 생성하여 현재 상태를 저장
+        public Memento SaveState()
+        {
+            return new Memento(Level, Health, Score);
+        }
+
+        // Memento 객체로부터 저장된 상태를 복원
+        public void RestoreState(Memento memento)
+        {
+            Level = memento.Level;
+            Health = memento.Health;
+            Score = memento.Score;
+        }
+    }
+
+    // Memento 클래스
+    [Serializable]
+    class Memento
+    {
+        public int Level { get; private set; }
+        public int Health { get; private set; }
+        public int Score { get; private set; }
+
+        public Memento(int level, int health, int score)
+        {
+            Level = level;
+            Health = health;
+            Score = score;
+        }
+    }
+
+    // Caretaker 클래스
+    class SaveManager
+    {
+        private string filePath = "save.dat";
+
+        // 현재 게임 상태를 저장
+        public void Save(Game game)
+        {
+            Memento memento = game.SaveState();
+            using (FileStream fs = new FileStream(filePath, FileMode.Create))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(fs, memento);
+            }
+            Console.WriteLine("Game saved.");
+        }
+
+        // 저장된 게임 상태를 불러옴
+        public void Load(Game game)
+        {
+            if (File.Exists(filePath))
+            {
+                using (FileStream fs = new FileStream(filePath, FileMode.Open))
+                {
+                    BinaryFormatter bf = new BinaryFormatter();
+                    Memento memento = (Memento)bf.Deserialize(fs);
+                    game.RestoreState(memento);
+                }
+                Console.WriteLine("Game loaded.");
+            }
+            else
+            {
+                Console.WriteLine("No save file found.");
+            }
+        }
     }
 }
