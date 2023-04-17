99b42a91bbc268501ef4dda5bf209f7b7449654a
동윤 김 <ut_community@naver.com>
2023-04-17 16:20:01 +0900

    feat : MementoPattern Use
    메멘토 패턴을 Main에서 사용할 수 있게 만들었습니다.

--- DesignPatternStudy/Program.cs
+++ DesignPatternStudy/Program.cs
@@ -6,7 +6,32 @@ namespace DesignPatternStudy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /* Memento Pattern */
+            /****************************************************************************************************/
+            // 게임 시작
+            Game game = new Game();
+            game.Level = 1;
+            game.Health = 100;
+            game.Score = 0;
+
+            // 저장 매니저 생성
+            SaveManager saveManager = new SaveManager();
+
+            // 게임 상태 저장
+            saveManager.Save(game);
+
+            // 게임 상태 변경
+            game.Level = 2;
+            game.Health = 50;
+            game.Score = 100;
+
+            game.DisplayStatus(); // Level: 2, Health: 50, Score: 100
+
+            // 게임 상태 복원
+            saveManager.Load(game);
+
+            game.DisplayStatus(); // Level: 1, Health: 100, Score: 0
+            /****************************************************************************************************/
         }
     }
 }
